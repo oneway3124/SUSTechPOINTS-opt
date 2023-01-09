b8da0cb89608fa3e3cdd065d13ea2dd01d9f4ccc
oneway3124 <190025935@qq.com>
2023-01-09 10:56:40 +0800

    增加复制脚本和数据集

---
+++ data/copy_lidar_label_2_SUSTechPoints_show.bat
@@ -0,0 +1,2 @@
+xcopy /y/s/e/h D:\labelCloud-opt\data\kitti_2_sustech_example\        D:\SUSTechPOINTS-opt\data\kitti_2_sustech_example\
+pause 
---
+++ data/rename_sustechlabel_2_label.bat
@@ -0,0 +1,3 @@
+ren D:\SUSTechPOINTS-opt\data\kitti_2_sustech_example\sustech_label  label
+rd /s /q D:\SUSTechPOINTS-opt\data\kitti_2_sustech_example\kitti_label                
+pause 
