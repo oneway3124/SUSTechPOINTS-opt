461403d91895e42f5c6ec20ef25de4d55d8d6948
oneway3124 <190025935@qq.com>
2023-02-02 14:28:19 +0800

    add 2 bat file script for dataset handle

---
+++ file_bat/minikitti_sustech-SUSTechPoints-opt2mmdetection3d-opt.bat
@@ -0,0 +1,6 @@
+xcopy /y/s/e/h C:\Users\lenovo\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\lidar\ C:\Users\lenovo\Desktop\mmdetection3d-opt\data\minikitti_sustech\bins\
+xcopy /y/s/e/h C:\Users\lenovo\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\lidar\ C:\Users\lenovo\Desktop\mmdetection3d-opt\data\minikitti_sustech\training\velodyne\
+xcopy /y/s/e/h C:\Users\lenovo\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\label\ C:\Users\lenovo\Desktop\mmdetection3d-opt\data\minikitti_sustech\labels\
+xcopy /y/s/e/h C:\Users\lenovo\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\label\ C:\Users\lenovo\Desktop\mmdetection3d-opt\data\minikitti_sustech\training\label\
+
+pause 
---
+++ file_bat/minikitti_sustech-mmdetection3d-opt2SUSTechPoints-opt.bat
@@ -0,0 +1,5 @@
+xcopy /y/s/e/h C:\Users\lenovo\Desktop\mmdetection3d-opt\data\minikitti_sustech\bins\  C:\Users\lenovo\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\lidar\ 
+
+xcopy /y/s/e/h C:\Users\lenovo\Desktop\mmdetection3d-opt\data\minikitti_sustech\labels\ C:\Users\lenovo\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\label\ 
+
+pause 
