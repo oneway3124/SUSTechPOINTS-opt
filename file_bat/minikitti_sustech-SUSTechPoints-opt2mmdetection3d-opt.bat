5557cea7d726026abe220ed1e97c1a68ba3ffe98
oneway3124 <190025935@qq.com>
2023-02-03 09:17:35 +0800

    add bat

--- file_bat/minikitti_sustech-SUSTechPoints-opt2mmdetection3d-opt.bat
+++ file_bat/minikitti_sustech-SUSTechPoints-opt2mmdetection3d-opt.bat
@@ -2,5 +2,8 @@ xcopy /y/s/e/h C:\Users\19002\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\l
 xcopy /y/s/e/h C:\Users\19002\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\lidar\ C:\Users\19002\Desktop\mmdetection3d-opt\data\minikitti_sustech\training\velodyne\
 xcopy /y/s/e/h C:\Users\19002\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\label\ C:\Users\19002\Desktop\mmdetection3d-opt\data\minikitti_sustech\labels\
 xcopy /y/s/e/h C:\Users\19002\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\label\ C:\Users\19002\Desktop\mmdetection3d-opt\data\minikitti_sustech\training\label\
+xcopy /y/s/e/h C:\Users\19002\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\SampleSets\ C:\Users\19002\Desktop\mmdetection3d-opt\data\minikitti_sustech\SampleSets\
+copy C:\Users\19002\Desktop\SUSTechPOINTS-opt\data\minikitti_sustech\classnames.txt C:\Users\19002\Desktop\mmdetection3d-opt\data\minikitti_sustech\classnames.txt
+
 
 pause 
